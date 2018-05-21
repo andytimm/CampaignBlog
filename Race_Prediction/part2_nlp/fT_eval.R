748921ad7ebb1324339aa74496d588e33c0b050e
Andrew Timm <timmandrew1@gmail.com>
2018-05-21 17:04:46 -0500

    hackish solution to clean fT predictions

--- Race_Prediction/part2_nlp/fT_eval.R
+++ Race_Prediction/part2_nlp/fT_eval.R
@@ -26,10 +26,24 @@ for(unique_value in unique(true_labels$race)){
 fT_baseline <- read_delim("C:\\Users\\Andy\\Documents\\Datasets\\Florida Voters\\fT_baseline_preds.txt",
                           delim =  " ", col_names = FALSE) %>% 
   mutate(i = row_number()) %>% 
-  spread(key = X1, value = X2) %>%
-  spread(key = X3, value = X4) %>%
-  spread(key = X5, value = X6) %>%
-  spread(key = X7, value = X8) %>%
-  spread(key = X9, value = X10)
+  spread(key = X1, value = X2, sep = "a") %>%
+  spread(key = X3, value = X4, sep = "b") %>%
+  spread(key = X5, value = X6, sep = "c") %>%
+  spread(key = X7, value = X8, sep = "d") %>%
+  spread(key = X9, value = X10, sep = "e") %>% 
+  
+  unite("fT_pred_white", c("X1a__label__white","X3b__label__white","X5c__label__white",
+                             "X7d__label__white","X9e__label__white")) %>% 
+  unite("fT_pred_black", c("X1a__label__black","X3b__label__black","X5c__label__black",
+                           "X7d__label__black","X9e__label__black")) %>% 
+  unite("fT_pred_hispanic", c("X1a__label__hispanic","X3b__label__hispanic","X5c__label__hispanic",
+                           "X7d__label__hispanic","X9e__label__hispanic")) %>% 
+  unite("fT_pred_asian", c("X1a__label__asian","X3b__label__asian","X5c__label__asian",
+                           "X7d__label__asian","X9e__label__asian")) %>% 
+  unite("fT_pred_other", c("X1a__label__other","X3b__label__other","X5c__label__other",
+                           "X7d__label__other","X9e__label__other")) %>%
+  select(-i)
+
+
 
 
